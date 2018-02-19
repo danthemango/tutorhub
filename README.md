81084bbbbd808a303ab4359ddfe085ddfda9ac40
danthemango <danthemango@gmail.com>
2018-02-19 01:14:24 -0800

    updated README to discuss new directory structure

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # tutorhub
 The student - tutor matchup website
 
+## pages
+- index.php:   front page of tutorhub
+- search.php:  get search results
+- profile.php: view and edit your profile
+
 ## directories
-- res: resources. Used to store bootstrap and other framework / library files
-- inc: inclusions. Used for files which will be included by other files.
+- res: resources. Any libraries and frameworks (e.g. jQuery, Bootstrap)
+- inc: inclusions. Php files which aren't full pages (i.e. headers, footers, database-related)
+- img: images
+- css: styling
+- js:  frontend code
--- inc/header.php
+++ inc/header.php
@@ -14,7 +14,7 @@
       <meta charset="UTF-8">
       <title><?= $sitetitle ?></title>
       <link rel="stylesheet" href="/~guenthdd/tutorhub/res/bootstrap.min.css">
-      <link rel="stylesheet" href="/~guenthdd/tutorhub/res/main.css">
+      <link rel="stylesheet" href="/~guenthdd/tutorhub/css/main.css">
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
       <script src="/~guenthdd/tutorhub/res/bootstrap.min.js"></script>
