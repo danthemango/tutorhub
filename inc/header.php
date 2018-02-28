dbf098a9689d72cfead1de213f7d210ae742a552
Daniel Dietrich Guenther <guenthdd@stumail.csci.viu.ca>
2018-02-28 00:41:54 -0800

    copied popper.js and jquery.js into res

--- inc/header.php
+++ inc/header.php
@@ -15,9 +15,9 @@
       <title><?= $sitetitle ?></title>
       <link rel="stylesheet" href="/~guenthdd/tutorhub/res/bootstrap.min.css">
       <link rel="stylesheet" href="/~guenthdd/tutorhub/css/main.css">
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="/~guenthdd/tutorhub/res/bootstrap.min.js"></script>
+      <script src="/~guenthdd/tutorhub/res/jquery-3.3.1.min.js" ></script>
+      <script src="/~guenthdd/tutorhub/res/popper.min.js" ></script>
+      <script src="/~guenthdd/tutorhub/res/bootstrap.min.js" ></script>
    </head>
    <body>
       <header>
