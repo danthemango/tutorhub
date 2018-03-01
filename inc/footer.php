2df94ec8da8bd7ab71347fd95a27a9dd27fa3d7e
danthemango <danthemango@gmail.com>
2018-03-01 01:39:19 -0800

    fixed minor DOM mistakes

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,12 @@
 <!-- start of footer -->
       <footer>
          <p>Created by The SCAD Group for CSCI 311 at VIU &copy; 2018</p>
+      </footer>
       <!-- Optional JavaScript -->
 
       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
       <script src="/~guenthdd/tutorhub/res/jquery-3.3.1.min.js" ></script>
       <script src="/~guenthdd/tutorhub/res/popper.min.js" ></script>
       <script src="/~guenthdd/tutorhub/res/bootstrap.min.js" ></script>
-      </footer>
    </body>
 </html>
