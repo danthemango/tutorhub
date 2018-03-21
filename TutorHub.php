cb38043e0101c0881b0b87a832c86b9a1e9bcba9
danthemango <danthemango@gmail.com>
2018-03-20 17:05:46 -0700

    using associative arrays for the tutorhub search

--- TutorHub.php
+++ TutorHub.php
@@ -1,13 +1,13 @@
 <!-- Created by Sami Al-Qusus March 3, 2018-->
 <!-- modified March 16, 2018 -->
 <!-- TutorHub.html required for TutorHub project -->
-<?php 
+<?php
 $pagetitle = "Welcome";
-require 'inc/header.php'; 
+require 'inc/header.php';
 ?>
 
-<div data-spy="scroll" data-offset="0">   
-   <section id="main">
+<div data-spy="scroll" data-offset="0">
+   <section id="container-fluid main h-100">
    <div class="jumbotron"><br><br><br><br>
     <h1 class="display-3">Welcome</h1><br><br><br>
     <p class="lead">TutorHub is a simple tutor finding service. Want some help in a class you're taking now? See if anyone is available:</p>
@@ -17,7 +17,7 @@ require 'inc/header.php';
    <section id="section1"><br><br><br><br>
     <form class="text-center" method="post" action="action.php">
       <label for="course" ><b>Pick the course you would like to find a tutor for:</b></label><br>
-      <select multiple id="course" name="course[]">
+      <select multiple id="course" name="courses[]">
         <option value="CSCI112">CSCI 112 - (Applications Programming)</option>
         <option value="CSCI115">CSCI 115 - (Web Page Techniques)</option>
         <option value="CSCI160">CSCI 160 - (Computing Science I)</option>
@@ -42,38 +42,38 @@ require 'inc/header.php';
        <tbody>
        <tr>
 	   <th scope="row">Monday</th>
-	   <td><input id="monAM" type="checkbox" name="monAM" value="monAM">Morning</td>
-	   <td><input id="monPM" type="checkbox" name="monPM" value="monPM">Afternoon</td>
+	   <td><input id="monAM" type="checkbox" name="times[monday]" value="AM">Morning</td>
+	   <td><input id="monPM" type="checkbox" name="times[monday]" value="PM">Afternoon</td>
        </tr>
        <tr>
 	   <th scope="row">Tuesday</th>
-           <td><input id="tueAM" type="checkbox" name="tueAM" value="tueAM">Morning</td>
-           <td><input id="tuePM" type="checkbox" name="tuePM" value="tuePM">Afternoon</td>
+           <td><input id="tueAM" type="checkbox" name="times[tuesday]" value="AM">Morning</td>
+           <td><input id="tuePM" type="checkbox" name="times[tuesday]" value="PM">Afternoon</td>
        </tr>
-       <tr> 
+       <tr>
            <th scope="row">Wednesday</th>
-           <td><input id="wedAM" type="checkbox" name="wedAM" value="wedAM">Morning</td>
-           <td><input id="wedPM" type="checkbox" name="wedPM" value="wedPM">Afternoon</td>
+           <td><input id="wedAM" type="checkbox" name="times[wednesday]" value="AM">Morning</td>
+           <td><input id="wedPM" type="checkbox" name="times[wednesday]" value="PM">Afternoon</td>
        </tr>
        <tr>
            <th scope="row">Thursday</th>
-           <td><input id="thurAM" type="checkbox" name="thurAM" value="thurAM">Morning</td>
-           <td><input id="thurPM" type="checkbox" name="thurPM" value="thurPM">Afternoon</td>
+           <td><input id="thurAM" type="checkbox" name="times[thursday]" value="AM">Morning</td>
+           <td><input id="thurPM" type="checkbox" name="times[thursday]" value="PM">Afternoon</td>
        </tr>
        <tr>
            <th scope="row">Friday</th>
-           <td><input id="friAM" type="checkbox" name="friAM" value="friAM">Morning</td>
-           <td><input id="friPM" type="checkbox" name="friPM" value="friPM">Afternoon</td>
+           <td><input id="friAM" type="checkbox" name="times[friday]" value="AM">Morning</td>
+           <td><input id="friPM" type="checkbox" name="times[friday]" value="PM">Afternoon</td>
        </tr>
        <tr>
            <th scope="row">Satursday</th>
-           <td><input id="satAM" type="checkbox" name="satAM" value="satAM">Morning</td>
-           <td><input id="satPM" type="checkbox" name="satPM" value="satPM">Afternoon</td>
+           <td><input id="satAM" type="checkbox" name="times[satursday]" value="AM">Morning</td>
+           <td><input id="satPM" type="checkbox" name="times[satursday]" value="PM">Afternoon</td>
        </tr>
        <tr>
            <th scope="row">Sunday</th>
-           <td><input id="sunAM" type="checkbox" name="sunAM" value="sunAM">Morning</td>
-           <td><input id="sunPM" type="checkbox" name="sunPM" value="sunPM">Afternoon</td>
+           <td><input id="sunAM" type="checkbox" name="times[sunday]" value="AM">Morning</td>
+           <td><input id="sunPM" type="checkbox" name="times[sunday]" value="PM">Afternoon</td>
        </tr>
        </tbody>
      </thead>
