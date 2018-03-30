721f8bdfe1df6e0c54c7de41f155c4809232fedc
Sari Angelo Ng <ngs@stumail.csci.viu.ca>
2018-03-30 15:15:43 -0700

    Added login/logout functionality, as well as helper functions for auth

--- login.php
+++ login.php
@@ -7,14 +7,51 @@
 <?php
 
 $pagetitle = "Tutor Login";
-require 'inc/header.php';
+require_once("inc/auth.php");
+require_once("inc/header.php");
+
+$login_error = false;
+if ($session) {
+   header("location:index.php");
+} else if (isset($_POST['submit'])) {
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   $info = attemptLogin($email, $password);
+
+   if ($info) {
+      $_SESSION['UserData']['Username'] = htmlspecialchars($info[0]);
+      $_SESSION['UserData']['Avatar'] = htmlspecialchars($info[1]);
+      header("location:index.php");
+   } else {
+      $login_error = true;
+   }
+}
+
+if ($login_error) {
+   echo "
+      <div id='login-error-modal' class='modal fade'>
+         <div class='modal-dialog modal-dialog-centered'>
+            <div class='modal-content alert alert-danger'>
+               <div class='modal-header'>
+                  <div>Login Failed</div>
+                  <button type='button' class='close' data-dismiss='modal'>&times;</button>
+               </div>
+               <div class='modal-body'>
+                  Invalid login credentials. Please try again.
+               </div>
+            </div>
+         </div>
+      </div>
+   ";
+   echo "<script>$('#login-error-modal').modal('show');</script>";
+}
 
 ?>
 
 <section class="container-fluid main h-100 with-background">
    <div class="row h-100 justify-content-center align-items-center">
       <div class="col-lg-4 col-md-8 col-xxs-12">
-         <form class="form-whitebox">
+         <form class="form-whitebox" method="post">
             <h4 class="text-center">Tutor Sign In</h4>
             <div class="form-group">
                <label for="email">Email</label>
@@ -23,10 +60,6 @@ require 'inc/header.php';
             <div class="form-group">
                <label for="password">Password</label>
                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <div class="form-check">
-               <input type="checkbox" class="form-check-input" id="remember" name="remember">
-               <label class="form-check-label" for="remember">Remember me</label>
             </div>
             <br>
             <div class="text-center mt-3">
