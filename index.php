49734eb109edc6e10c2ebb1dd024b2cf2e107a60
Sami Al Qusus <alqususs@stumail.csci.viu.ca>
2018-03-30 15:55:43 -0700

    index.php fix: course values to small letters

--- index.php
+++ index.php
@@ -35,22 +35,22 @@ $PM =1;
     <form class="text-center" method="post" action="search.php">
       <label for="course" ><b>Pick the course you would like to find a tutor for:</b></label><br>
       <select multiple id="courses" name="courses[]">
-        <option value="CSCI112">CSCI 112 - (Applications Programming)</option>
-        <option value="CSCI115">CSCI 115 - (Web Page Techniques)</option>
-        <option value="CSCI160">CSCI 160 - (Computing Science I)</option>
-        <option value="CSCI161">CSCI 161 - (Computing Science II)</option>
-        <option value="CSCI162">CSCI 162 - (Topics in Computing Science)</option>
-        <option value="CSCI261">CSCI 261 - (Computer Architecture and Assembly Language)</option>
-        <option value="MATH121">MATH 121 - (Calculus I)</option>
-        <option value="MATH123">MATH 123 - (Logic and Foundations)</option>
-        <option value="CSCI251">CSCI 251 - (Systems and Networks)</option>
-        <option value="CSCI260">CSCI 260 - (Data Structures)</option>
-        <option value="CSCI265">CSCI 265 - (Software Engineering)</option>
-        <option value="CSCI310">CSCI 310 - (Intro to Graphical User Interfaces)</option>
-        <option value="CSCI331">CSCI 331 - (Object Oriented Programming)</option>
-        <option value="CSCI370">CSCI 370 - (Database Systems)</option>
-        <option value="CSCI375">CSCI 375 - (Intro to Systems Analysis)</option>
-        <option value="CSCI400">CSCI 400 - (Computers and Society)</option>
+        <option value="csci112">CSCI 112 - (Applications Programming)</option>
+        <option value="csci115">CSCI 115 - (Web Page Techniques)</option>
+        <option value="csci160">CSCI 160 - (Computing Science I)</option>
+        <option value="csci161">CSCI 161 - (Computing Science II)</option>
+        <option value="csci162">CSCI 162 - (Topics in Computing Science)</option>
+        <option value="csci261">CSCI 261 - (Computer Architecture and Assembly Language)</option>
+        <option value="csci121">MATH 121 - (Calculus I)</option>
+        <option value="csci123">MATH 123 - (Logic and Foundations)</option>
+        <option value="csci251">CSCI 251 - (Systems and Networks)</option>
+        <option value="csci260">CSCI 260 - (Data Structures)</option>
+        <option value="csci265">CSCI 265 - (Software Engineering)</option>
+        <option value="csci310">CSCI 310 - (Intro to Graphical User Interfaces)</option>
+        <option value="csci331">CSCI 331 - (Object Oriented Programming)</option>
+        <option value="csci370">CSCI 370 - (Database Systems)</option>
+        <option value="csci375">CSCI 375 - (Intro to Systems Analysis)</option>
+        <option value="csci400">CSCI 400 - (Computers and Society)</option>
       </select><br><br>
       <p><b>Check all the times that work for you:</b> &nbsp;
       <input id="day" type="checkbox" name="day" value="any">All</p>
