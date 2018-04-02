d9bbb549e12a2b03f23e4cebce45eaf03de05a52
danthemango <danthemango@gmail.com>
2018-04-01 23:13:19 -0700

    js/search.js - utilizing the jqs('reset') and jqs('import') methods
    - I read through the documentation and realized these were the methods
    required to reset the jquery scheduler
    - Took out the jquery deletion / creation section
    - the form is still copying all schedule data over to the modal for each
    schedule draw, and it might be better to later create just a reference
    to this information

--- js/search.js
+++ js/search.js
@@ -6,44 +6,28 @@
  * Modified: -
  */
 
-
-// returns jquery schedule object to be drawn, given date data
-function getJqs(data){
-   var result = {
-      mode: "read",
-      // shortens the name displayed on the week list (e.g. Mo instead of Monday)
-      days: ["Mo","Tu","We","Th","Fr","Sa","Su"],
-      hour: 24,
-      data: data,
-   };
-   return result;
-}
-
 // run this only after page load
 $(document).ready(function(){
    // once the user clicks on a button we want to specify which schedule to draw
    $('button[data-userid]').on('click',function(){
-      // tell the scheduleModal which user we want to display
-      $('#scheduleModal').data('userid',$(this).data('userid'));
-      $('#scheduleModal').data('firstname',$(this).data('firstname'));
-      $('#scheduleModal').data('lastname',$(this).data('lastname'));
+      // change the title of the modal
+      $('#scheduleModal .modal-title').text("Availabilities for "+$(this).data('firstname')+' '+$(this).data('lastname'));
+      // put the schedule information in the modal
       $('#scheduleModal').data('schedule',$(this).data('schedule'));
    });
 
-   // draw the schedule once the user requests it
+   // draw the schedule once the modal actually shows up
    $('#scheduleModal').on('shown.bs.modal',function(){
-      // change the title of the page
-      $('#scheduleModal .modal-title').text("Availabilities for "+$(this).data('firstname')+' '+$(this).data('lastname'));
-      // set the schedule of the modal in a child
-      $('#scheduleModal .modal-body').append('<div id=\'schedule\'></div>');
-      $('#schedule').jqs(getJqs($(this).data('schedule')));
-   });
-
-   // take the scheduleChild away after the user leaves modal 
-   // (to force it to draw a different one next time)
-   $('#scheduleModal').on('hidden.bs.modal',function(){
-      // delete the schedule from the modal
-      $('#scheduleModal .modal-body').html('');
+      if($('#schedule').length < 1){
+         $('#scheduleModal .modal-body').append('<div id=\'schedule\'></div>');
+         $('#schedule').jqs({
+            mode: "read",
+            // shortens the name displayed on the week list (e.g. Mo instead of Monday)
+            days: ["Mo","Tu","We","Th","Fr","Sa","Su"],
+            hour: 24,
+         });
+      }
+      $('#schedule').jqs('reset');
+      $('#schedule').jqs('import',$(this).data('schedule'));
    });
 });
-
