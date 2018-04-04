329de8a1a3140665d0b191dbd551bf10d9b9d5be
danthemango <danthemango@gmail.com>
2018-04-03 18:09:36 -0700

    added passwords to dummy profiles

--- ops/sql/01-insertDummies.sql
+++ ops/sql/01-insertDummies.sql
@@ -6,39 +6,40 @@
  */
 
 -- creating dummy profiles
-insert into profiles(email,firstname,lastname,phone,date_joined,avatar) values ("danthemango@tutorhub.com", "Daniel","Guenther",5555555555,NOW(),"card_snowydan.jpg");
-insert into profiles(email,firstname,lastname,phone,date_joined,avatar) values ("neptunny@tutorhub.com", "Nep","Tune",5555555556,NOW(),"card_neptune.jpg");
-insert into profiles(email,firstname,lastname,phone,date_joined,avatar) values ("yourbuddhy@tutorhub.com", "Budd","Ha",5555555557,NOW(),"card_thai_statue.jpg");
-insert into profiles(email,firstname,lastname,phone,date_joined,avatar) values ("palmy@tutorhub.com", "Palm","Trees",5555555558,NOW(),"card_palm_tree.jpg");
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("jdoe@tutorhub.com", "John","Doe",5555555559,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello1@world1.com", "hello1","world1",5555555560,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello2@world2.com", "hello2","world2",5555555561,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello3@world3.com", "hello3","world3",5555555562,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello4@world4.com", "hello4","world4",5555555563,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello5@world5.com", "hello5","world5",5555555564,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello6@world6.com", "hello6","world6",5555555565,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello7@world7.com", "hello7","world7",5555555566,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello8@world8.com", "hello8","world8",5555555567,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello9@world9.com", "hello9","world9",5555555568,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello10@world10.com", "hello10","world10",5555555569,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello11@world11.com", "hello11","world11",5555555570,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello12@world12.com", "hello12","world12",5555555571,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello13@world13.com", "hello13","world13",5555555572,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello14@world14.com", "hello14","world14",5555555573,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello15@world15.com", "hello15","world15",5555555574,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello16@world16.com", "hello16","world16",5555555575,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello17@world17.com", "hello17","world17",5555555576,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello18@world18.com", "hello18","world18",5555555577,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello19@world19.com", "hello19","world19",5555555578,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello20@world20.com", "hello20","world20",5555555579,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello21@world21.com", "hello21","world21",5555555580,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello22@world22.com", "hello22","world22",5555555581,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello23@world23.com", "hello23","world23",5555555582,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello24@world24.com", "hello24","world24",5555555583,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello25@world25.com", "hello25","world25",5555555584,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello26@world26.com", "hello26","world26",5555555585,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello27@world27.com", "hello27","world27",5555555586,NOW());
-insert into profiles(email,firstname,lastname,phone,date_joined) values ("hello28@world28.com", "hello28","world28",5555555587,NOW());
+-- (note: they all have the password:'helloworld')
+insert into profiles(email,firstname,lastname,phone,date_joined,password,avatar) values ("danthemango@tutorhub.com", "Daniel","Guenther",5555555555,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa","card_snowydan.jpg");
+insert into profiles(email,firstname,lastname,phone,date_joined,password,avatar) values ("neptunny@tutorhub.com", "Nep","Tune",5555555556,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa","card_neptune.jpg");
+insert into profiles(email,firstname,lastname,phone,date_joined,password,avatar) values ("yourbuddhy@tutorhub.com", "Budd","Ha",5555555557,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa","card_thai_statue.jpg");
+insert into profiles(email,firstname,lastname,phone,date_joined,password,avatar) values ("palmy@tutorhub.com", "Palm","Trees",5555555558,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa","card_palm_tree.jpg");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("jdoe@tutorhub.com", "John","Doe",5555555559,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello1@world1.com", "hello1","world1",5555555560,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello2@world2.com", "hello2","world2",5555555561,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello3@world3.com", "hello3","world3",5555555562,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello4@world4.com", "hello4","world4",5555555563,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello5@world5.com", "hello5","world5",5555555564,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello6@world6.com", "hello6","world6",5555555565,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello7@world7.com", "hello7","world7",5555555566,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello8@world8.com", "hello8","world8",5555555567,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello9@world9.com", "hello9","world9",5555555568,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello10@world10.com", "hello10","world10",5555555569,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello11@world11.com", "hello11","world11",5555555570,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello12@world12.com", "hello12","world12",5555555571,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello13@world13.com", "hello13","world13",5555555572,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello14@world14.com", "hello14","world14",5555555573,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello15@world15.com", "hello15","world15",5555555574,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello16@world16.com", "hello16","world16",5555555575,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello17@world17.com", "hello17","world17",5555555576,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello18@world18.com", "hello18","world18",5555555577,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello19@world19.com", "hello19","world19",5555555578,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello20@world20.com", "hello20","world20",5555555579,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello21@world21.com", "hello21","world21",5555555580,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello22@world22.com", "hello22","world22",5555555581,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello23@world23.com", "hello23","world23",5555555582,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello24@world24.com", "hello24","world24",5555555583,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello25@world25.com", "hello25","world25",5555555584,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello26@world26.com", "hello26","world26",5555555585,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello27@world27.com", "hello27","world27",5555555586,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
+insert into profiles(email,firstname,lastname,phone,date_joined,password) values ("hello28@world28.com", "hello28","world28",5555555587,NOW(),"$2y$10$YmRlODQ0YTBiM2ExNmY5MOo/tw0GEmOoS/msX4uw/q9xpaCZT8IWa");
 
 -- insert a list of classes
 insert into classes(code, description) values("csci160", "Computer Science I");
